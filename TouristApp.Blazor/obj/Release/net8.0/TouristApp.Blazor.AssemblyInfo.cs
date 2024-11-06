4d79bd5c42d1ee47b07d08283b96a262311884ca
Nicholas <80521978+Fioletich@users.noreply.github.com>
2024-11-06 10:01:35 +0300

    Fixed bugs

--- TouristApp.Blazor/obj/Release/net8.0/TouristApp.Blazor.AssemblyInfo.cs
+++ TouristApp.Blazor/obj/Release/net8.0/TouristApp.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TouristApp.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055e2712cd386df09be3097877d79c57f0347e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("TouristApp.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TouristApp.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
